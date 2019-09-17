3ca9e1fa97a4f97c87f58433f99c003300631c13
andrerim <andreasrim@gmail.com>
2019-09-18 00:03:14 +0200

    Added a very basic form of word cloud. Font changes based on the occurence of a term in the input

--- src/countWords.jsx
+++ src/countWords.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
+
+var div;
+
+
+
+// To do: Add checks for submiting an empty input  
 class CountWords extends React.Component{
     constructor(props){
         super(props);
@@ -14,6 +20,7 @@ class CountWords extends React.Component{
         this.displayWordCounts = this.displayWordCounts.bind(this);
     }
 
+    
 
     updateWordCount(event){
         this.setState({words: event.target.value});
@@ -27,17 +34,39 @@ class CountWords extends React.Component{
         let words = this.state.words.split(/\W+/)
 
         let wordCounts = {};
+        let keys = [];
         for (let i = 0; i < words.length; i++){
             let word = words[i]
             if (wordCounts[word]){
                 wordCounts[word]++;
+                
             } else{
                 wordCounts[word] = 1;
+                keys.push(word);
             }
         };
+
+
         console.log(wordCounts);
+        console.log(keys);
+        
+        let newDiv = []
         
         
+        for (let i = 0; i < keys.length; i++){
+            let divStyle = {
+            fontSize: 1,
+            padding: 0,
+            margin: 0,
+            };  
+            console.log("wordCount: " + wordCounts[keys[i]]);
+            
+            divStyle["fontSize"] = wordCounts[keys[i]]*10; 
+            console.log("DivStyle: " + divStyle["fontSize"]);
+            newDiv.push(<p style={divStyle} key={i} >{keys[i]}</p>);
+        };
+
+        this.setState({counts: newDiv});
 
     }
 
@@ -53,7 +82,6 @@ class CountWords extends React.Component{
             </form>
 
             <div className="output">
-                 {this.state.words}
                  {this.state.counts}
             </div>
             </div>
